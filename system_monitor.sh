3e917a8389ab879ff629c6d5a00f9566c8b84fb1
AryapratapNayak <aryapratapnayak2005@gmail.com>
2026-07-01 05:49:38 +0000

    Enhanced disk monitoring with threshold alerts

--- system_monitor.sh
+++ system_monitor.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
+#Maintainer DIM Team May 2026
+#Date 1st July 2026
 
+# Displaying basic system information
 echo "======================================"
 echo "      SYSTEM MONITORING SCRIPT"
 echo "======================================"
@@ -10,6 +13,21 @@ echo "Current User : $(whoami)"
 echo "Date : $(date)"
 echo "Uptime : $(uptime -p)"
 
+# Monitoring disk usage
 echo
 echo "========== Disk Usage =========="
-df -h /
+
+DISK_THRESHOLD=80
+
+disk_usage=$(df / | awk 'NR==2 {print $5}' | sed 's/%//')
+
+echo "Current Disk Usage : ${disk_usage}%"
+
+if [ "$disk_usage" -gt "$DISK_THRESHOLD" ]
+then
+    echo "ALERT: Disk usage has exceeded ${DISK_THRESHOLD}%!"
+else
+    echo "Disk usage is within the safe limit."
+fi
+
+#Monitoring memory usage
