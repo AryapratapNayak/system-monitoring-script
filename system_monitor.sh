783dad36afdff5128e56d6a83b99687dee827f12
AryapratapNayak <aryapratapnayak2005@gmail.com>
2026-07-01 06:13:40 +0000

    Added alert logging to monitor log

--- system_monitor.sh
+++ system_monitor.sh
@@ -2,6 +2,8 @@
 #Maintainer DIM Team May 2026
 #Date 1st July 2026
 
+LOG_FILE="monitor.log"
+
 # Displaying basic system information
 echo "======================================"
 echo "      SYSTEM MONITORING SCRIPT"
@@ -17,7 +19,7 @@ echo "Uptime : $(uptime -p)"
 echo
 echo "========== Disk Usage =========="
 
-DISK_THRESHOLD=80
+DISK_THRESHOLD=40
 
 disk_usage=$(df / | awk 'NR==2 {print $5}' | sed 's/%//')
 
@@ -26,6 +28,7 @@ echo "Current Disk Usage : ${disk_usage}%"
 if [ "$disk_usage" -gt "$DISK_THRESHOLD" ]
 then
     echo "ALERT: Disk usage has exceeded ${DISK_THRESHOLD}%!"
+    echo "$(date) - ALERT: Disk usage is ${disk_usage}%." >> "$LOG_FILE"
 else
     echo "Disk usage is within the safe limit."
 fi
@@ -35,7 +38,7 @@ fi
 echo
 echo "========== Memory Usage =========="
 
-MEMORY_THRESHOLD=80
+MEMORY_THRESHOLD=5
 
 memory_usage=$(free | awk '/Mem/ {printf("%.0f"), $3/$2 * 100}')
 
@@ -44,6 +47,7 @@ echo "Current Memory Usage : ${memory_usage}%"
 if [ "$memory_usage" -gt "$MEMORY_THRESHOLD" ]
 then
     echo "ALERT: Memory usage has exceeded ${MEMORY_THRESHOLD}%!"
+    echo "$(date) - ALERT: Memory usage is ${memory_usage}%." >> "$LOG_FILE"
 else
     echo "Memory usage is within the safe limit."
 fi
