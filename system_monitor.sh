9439e1de7d89dcd0875ae23610339916966058b9
AryapratapNayak <aryapratapnayak2005@gmail.com>
2026-07-01 05:29:55 +0000

    Initial project setup

---
+++ system_monitor.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "======================================"
+echo "      SYSTEM MONITORING SCRIPT"
+echo "======================================"
+
+echo
+echo "Hostname : $(hostname)"
+echo "Current User : $(whoami)"
+echo "Date : $(date)"
+echo "Uptime : $(uptime -p)"
